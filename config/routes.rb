0304556ce53656dcaeeaae07aa9b2078a15f2fe3
Nico Proto <nicolasproto@gmail.com>
2019-09-26 23:31:56 +0200

    FEAT: Added routes for doses and cocktail methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   root to: 'cocktails#index'
+  resources :cocktails, only: [:show, :new, :create] do
+    resources :doses, only: [:new, :create]
+  end
+
+  resources :doses, only: [:destroy]
 end
