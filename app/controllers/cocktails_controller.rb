91f21cca6f5989b803cef750f171cf09c3d2a0d3
Nico Proto <nicolasproto@gmail.com>
2019-09-26 23:34:27 +0200

    FEAT: Added missing methods, styling and made the web responsive

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -5,5 +5,26 @@ class CocktailsController < ApplicationController
 
   def show
     @cocktail = Cocktail.find(params[:id])
+    @doses = @cocktail.doses
+    @dose = Dose.new
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.create(cocktail_strong_params)
+    if @cocktail.valid?
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def cocktail_strong_params
+    params.require(:cocktail).permit(:name, :photo)
   end
 end
